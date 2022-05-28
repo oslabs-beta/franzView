ea6356d1af34711e561f20fa740664c1fd761e3a
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-05-28 10:58:05 -0700

    Get Broker Information

---
+++ src/server/kafka/kafka.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: [process.env.KAKFA_BROKER],
+});
+
+const admin = kafka.admin();
+const { CONNECT } = admin.events;
+admin.on(CONNECT, () =>
+  console.log(`Kafka Admin Connected to ${process.env.KAKFA_BROKER}!`)
+);
+
+export { admin };
