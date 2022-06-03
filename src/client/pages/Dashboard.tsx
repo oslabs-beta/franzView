e8575d5d60e195d2e32c8800119e61abf4f0da7d
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-06-02 19:08:45 -0700

    Add dark mode

--- src/client/pages/Dashboard.tsx
+++ src/client/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -42,15 +41,13 @@ function Copyright(props: any) {
   );
 }
 
-const mdTheme = createTheme();
-
 function DashboardContent() {
   const { loading, error, data } = useQuery(CARD_METRICS_QUERY, {
     pollInterval: 60000,
   });
 
   return (
-    <ThemeProvider theme={mdTheme}>
+    <>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
         <Header />
@@ -78,6 +75,7 @@ function DashboardContent() {
                     display: "flex",
                     flexDirection: "column",
                   }}
+                  elevation={16}
                 >
                   <Chart
                     query={ALL_BROKER_CPU_USAGE}
@@ -98,6 +96,7 @@ function DashboardContent() {
                     display: "flex",
                     flexDirection: "column",
                   }}
+                  elevation={16}
                 >
                   <Chart
                     query={ALL_BROKER_DISK_USAGE}
@@ -119,6 +118,7 @@ function DashboardContent() {
                     flexDirection: "column",
                     height: 200,
                   }}
+                  elevation={8}
                 >
                   <MetricsCard
                     value={
@@ -141,6 +141,7 @@ function DashboardContent() {
                     flexDirection: "column",
                     height: 200,
                   }}
+                  elevation={8}
                 >
                   <MetricsCard
                     value={
@@ -162,6 +163,7 @@ function DashboardContent() {
                     flexDirection: "column",
                     height: 200,
                   }}
+                  elevation={8}
                 >
                   <MetricsCard
                     value={
@@ -176,7 +178,10 @@ function DashboardContent() {
               </Grid>
               {/* Broker Component */}
               <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                <Paper
+                  sx={{ p: 2, display: "flex", flexDirection: "column" }}
+                  elevation={4}
+                >
                   <Broker />
                 </Paper>
               </Grid>
@@ -190,7 +195,7 @@ function DashboardContent() {
           </Container>
         </Box>
       </Box>
-    </ThemeProvider>
+    </>
   );
 }
 
