8861cc2aa36bab18ffea425938d94ae4181f5894
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-06-02 17:06:45 -0700

    Removing old comments

--- src/client/components/Chart.tsx
+++ src/client/components/Chart.tsx
@@ -36,8 +36,6 @@ ChartJS.register(
   ChartStreaming
 );
 
-const animation = {};
-
 export default function Chart({
   query,
   metric,
@@ -55,42 +53,6 @@ export default function Chart({
   });
 
   const colors = ["00f5d4", "00bbf9", "9b5de5", "f15bb5", "fee440"];
-
-  // const delayBetweenPoints = 1000;
-  // const previousY = (ctx) =>
-  //   ctx.index === 0
-  //     ? ctx.chart.scales.y.getPixelForValue(100)
-  //     : ctx.chart
-  //         .getDatasetMeta(ctx.datasetIndex)
-  //         .data[ctx.index - 1].getProps(["y"], true).y;
-  // const animation = {
-  //   x: {
-  //     type: "string",
-  //     easing: "linear",
-  //     duration: delayBetweenPoints,
-  //     from: NaN, // the point is initially skipped
-  //     delay(ctx) {
-  //       if (ctx.type !== "data" || ctx.xStarted) {
-  //         return 0;
-  //       }
-  //       ctx.xStarted = true;
-  //       return ctx.index * delayBetweenPoints;
-  //     },
-  //   },
-  //   y: {
-  //     type: "number",
-  //     easing: "linear",
-  //     duration: delayBetweenPoints,
-  //     from: previousY,
-  //     delay(ctx) {
-  //       if (ctx.type !== "data" || ctx.yStarted) {
-  //         return 0;
-  //       }
-  //       ctx.yStarted = true;
-  //       return ctx.index * delayBetweenPoints;
-  //     },
-  //   },
-  // };
 
   const options: ChartOptions<"line"> = {
     responsive: true,
@@ -164,30 +126,6 @@ export default function Chart({
     fetchPolicy: "network-only",
     nextFetchPolicy: "network-only",
     notifyOnNetworkStatusChange: true,
-    // onCompleted: (data) => {
-    //   console.log(metric, "request completed");
-    //   if (chartRef.current?.data.datasets.length > 1 && loaded.current) {
-    //     data.brokers.forEach((broker, index) => {
-    //       broker[`${metric}OverTime`].forEach((point => broker[index]);
-    //     });
-
-    //     console.log(metric, "updating");
-    //     chartRef.current.update();
-    //   }
-
-    //   setTimeout(() => {
-    //     const variables = {
-    //       start: timeNow.current.toString(),
-    //       end: new Date(
-    //         timeNow.current.valueOf() + pollInterval * 1000
-    //       ).toString(),
-    //       step: step,
-    //     };
-    //     timeNow.current = new Date(variables.end);
-    //     refetch({ ...variables });
-    //   }, pollInterval * 1000);
-    //   return data;
-    // },
   });
 
   useEffect(() => {
@@ -201,18 +139,6 @@ export default function Chart({
       brokerData.borderColor = `#${colors[index]}`;
       brokerData.pointRadius = 0;
       brokerData.tension = 0.2;
-      // brokerData.data = broker[`${metric}OverTime`].map((point) => {
-      //   if (index === 0) {
-      //     labels.push(
-      //       new Date(point.time).toLocaleTimeString("en-us", {
-      //         hour: "2-digit",
-      //         minute: "2-digit",
-      //         second: "2-digit",
-      //       })
-      //     );
-      //   }
-      //   return point[metric];
-      // });
 
       brokerData.data = broker[`${metric}OverTime`];
 
