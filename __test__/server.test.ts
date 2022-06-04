20dc73c8a81ac654d6feb9fda7a250706edcb732
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-06-04 09:42:50 -0700

    Update server tests

--- __test__/server.test.ts
+++ __test__/server.test.ts
@@ -4,10 +4,6 @@ const server = "http://localhost:3000";
 
 describe("REST Server", () => {
   describe("404s for non-existant routes", () => {
-    it("Bad GET Request", () => {
-      return request(server).get("/badRoute").expect(404);
-    });
-
     it("Bad POST Request", () => {
       return request(server).post("/badRoute").expect(404);
     });
