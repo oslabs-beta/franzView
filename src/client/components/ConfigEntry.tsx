4c4d573928f60f67c71563505296482168394799
Patrick Reid <patrickjreid@gmail.com>
2022-07-22 17:40:06 -0600

    Made the config options buttons inline - some styling still required

--- src/client/components/ConfigEntry.tsx
+++ src/client/components/ConfigEntry.tsx
@@ -36,83 +36,87 @@ function ConfigEntry({ configs, setConfigs, index }) {
           "& .MuiTextField-root": { m: 1 },
         }}
       > */}
-
         <Grid item xs={4}>
-          <Box width="100%">
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">
-                Configuration Options
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={age}
-                label="Age"
-                name="config"
-                onChange={(event) => handleFormChange(index, event)}
-              >
-                <MenuItem value="cleanup.policy">cleanup.policy</MenuItem>
-                <MenuItem value="compression.type">compression.type</MenuItem>
-                <MenuItem value="delete.retention.ms">
-                  delete.retention.ms
-                </MenuItem>
-                <MenuItem value="file.delete.delay.ms">
-                  file.delete.delay.ms
-                </MenuItem>
-                <MenuItem value="flush.messages">flush.messages</MenuItem>
-                <MenuItem value="flush.ms">flush.ms</MenuItem>
-                <MenuItem value="follower.replication.throttled.replicas">
-                  follower.replication.throttled.replicas
-                </MenuItem>
-                <MenuItem value="index.interval.bytes">
-                  index.interval.bytes
-                </MenuItem>
-                <MenuItem value="leader.replication.throttled.replicas">
-                  leader.replication.throttled.replicas
-                </MenuItem>
-                <MenuItem value="max.compaction.lag.ms">
-                  max.compaction.lag.ms
-                </MenuItem>
-                <MenuItem value="max.message.bytes">max.message.bytes</MenuItem>
-                <MenuItem value="message.format.version">
-                  message.format.version
-                </MenuItem>
-                <MenuItem value="message.timestamp.difference.max.ms">
-                  message.timestamp.difference.max.ms
-                </MenuItem>
-                <MenuItem value="message.timestamp.type">
-                  message.timestamp.type
-                </MenuItem>
-                <MenuItem value="min.cleanable.dirty.ratio">
-                  min.cleanable.dirty.ratio
-                </MenuItem>
-                <MenuItem value="min.compaction.lag.ms">
-                  min.compaction.lag.ms
-                </MenuItem>
-                <MenuItem value="min.insync.replicas">
-                  min.insync.replicas
-                </MenuItem>
-                <MenuItem value="preallocate">preallocate</MenuItem>
-                <MenuItem value="retention.bytes">retention.bytes</MenuItem>
-                <MenuItem value="retention.ms">retention.ms</MenuItem>
-                <MenuItem value="segment.bytes">segment.bytes</MenuItem>
-                <MenuItem value="segment.index.bytes">
-                  segment.index.bytes
-                </MenuItem>
-                <MenuItem value="segment.jitter.ms">segment.jitter.ms</MenuItem>
-                <MenuItem value="segment.ms">segment.ms</MenuItem>
-                <MenuItem value="unclean.leader.election.enable">
-                  unclean.leader.election.enable
-                </MenuItem>
-                <MenuItem value="message.downconversion.enable">
-                  message.downconversion.enable
-                </MenuItem>
-                <MenuItem value="confluent.key.schema.validation">
-                  confluent.key.schema.validation
-                </MenuItem>
-              </Select>
-            </FormControl>
-
+          <Box width="100%" sx={{ display: "flex" }}>
+            <Grid item xs={4} justifyContent="flex-end">
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Configuration Options
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="Age"
+                  name="config"
+                  onChange={(event) => handleFormChange(index, event)}
+                >
+                  <MenuItem value="cleanup.policy">cleanup.policy</MenuItem>
+                  <MenuItem value="compression.type">compression.type</MenuItem>
+                  <MenuItem value="delete.retention.ms">
+                    delete.retention.ms
+                  </MenuItem>
+                  <MenuItem value="file.delete.delay.ms">
+                    file.delete.delay.ms
+                  </MenuItem>
+                  <MenuItem value="flush.messages">flush.messages</MenuItem>
+                  <MenuItem value="flush.ms">flush.ms</MenuItem>
+                  <MenuItem value="follower.replication.throttled.replicas">
+                    follower.replication.throttled.replicas
+                  </MenuItem>
+                  <MenuItem value="index.interval.bytes">
+                    index.interval.bytes
+                  </MenuItem>
+                  <MenuItem value="leader.replication.throttled.replicas">
+                    leader.replication.throttled.replicas
+                  </MenuItem>
+                  <MenuItem value="max.compaction.lag.ms">
+                    max.compaction.lag.ms
+                  </MenuItem>
+                  <MenuItem value="max.message.bytes">
+                    max.message.bytes
+                  </MenuItem>
+                  <MenuItem value="message.format.version">
+                    message.format.version
+                  </MenuItem>
+                  <MenuItem value="message.timestamp.difference.max.ms">
+                    message.timestamp.difference.max.ms
+                  </MenuItem>
+                  <MenuItem value="message.timestamp.type">
+                    message.timestamp.type
+                  </MenuItem>
+                  <MenuItem value="min.cleanable.dirty.ratio">
+                    min.cleanable.dirty.ratio
+                  </MenuItem>
+                  <MenuItem value="min.compaction.lag.ms">
+                    min.compaction.lag.ms
+                  </MenuItem>
+                  <MenuItem value="min.insync.replicas">
+                    min.insync.replicas
+                  </MenuItem>
+                  <MenuItem value="preallocate">preallocate</MenuItem>
+                  <MenuItem value="retention.bytes">retention.bytes</MenuItem>
+                  <MenuItem value="retention.ms">retention.ms</MenuItem>
+                  <MenuItem value="segment.bytes">segment.bytes</MenuItem>
+                  <MenuItem value="segment.index.bytes">
+                    segment.index.bytes
+                  </MenuItem>
+                  <MenuItem value="segment.jitter.ms">
+                    segment.jitter.ms
+                  </MenuItem>
+                  <MenuItem value="segment.ms">segment.ms</MenuItem>
+                  <MenuItem value="unclean.leader.election.enable">
+                    unclean.leader.election.enable
+                  </MenuItem>
+                  <MenuItem value="message.downconversion.enable">
+                    message.downconversion.enable
+                  </MenuItem>
+                  <MenuItem value="confluent.key.schema.validation">
+                    confluent.key.schema.validation
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
             <Grid item xs={4} justifyContent="flex-end">
               <TextField
                 id="outlined-basic"
