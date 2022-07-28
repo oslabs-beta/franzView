69dbc31c3947ef90ffcc131ba773c36ff9b5a624
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-07-27 18:34:00 -0700

    Adding button to topic grid for reassigning partitions

--- src/client/components/TopicGrid.tsx
+++ src/client/components/TopicGrid.tsx
@@ -5,71 +5,81 @@ import Title from "./Title";
 import { TOPIC_DATAGRID_QUERY, DELETE_TOPIC } from "../models/queries";
 import { useQuery } from "@apollo/client";
 import ConfirmationDialog from "./ConfirmationDialog";
+import Button from "@mui/material/Button";
+import { Navigate, useNavigate } from "react-router-dom";
+
 // onQueryCallback with use query
 
 // data grid schema
-const columns: GridColDef[] = [
-  { field: "topic", headerName: "Topic", width: 150 },
-  {
-    field: "partitionNum",
-    headerName: "Number of Partitions",
-    type: "number",
-    width: 150,
-  },
-  {
-    field: "partitionRep",
-    headerName: "Replicas per partition",
-    type: "number",
-    width: 150,
-  },
-  {
-    field: "underMinISR",
-    headerName: "Under Min ISR",
-    type: "number",
-    width: 150,
-  },
-  { field: "logSize", headerName: "Log Size(GB)", type: "number", width: 120 },
-  {
-    field: "delete",
-    headerName: "",
-    filterable: false,
-    align: "center",
-    width: 180,
-    renderCell: (params) => {
-      return params.value ? (
-        <ConfirmationDialog
-          title={`Delete ${params.row.topic}?`}
-          content={`Are you sure you want to delete topic: ${params.row.topic}? \n
-        Enter the topic name below to confirm. This is irreversible!`}
-          label="Delete"
-          actions={DELETE_TOPIC}
-          control={params.row.topic}
-          args={{ name: params.row.topic }}
-          variant="contained"
-          color="error"
-          cta="DELETE"
-          disabled={!params.value}
-          update={params.row.update}
-        />
-      ) : (
-        <ConfirmationDialog
-          title={`Delete ${params.row.topic}?`}
-          content={`Are you sure you want to delete topic: ${params.row.topic}? \n
-        Enter the topic name below to confirm. This is irreversible!`}
-          label="Delete"
-          actions={DELETE_TOPIC}
-          control={params.row.topic}
-          args={{ name: params.row.topic }}
-          variant="contained"
-          color="error"
-          cta="DELETE"
-          disabled={params.value}
-          update={params.row.update}
-        />
-      );
-    },
-  },
-];
+// const columns: GridColDef[] = [
+// 	{ field: "topic", headerName: "Topic", width: 150 },
+// 	{
+// 		field: "partitionNum",
+// 		headerName: "Number of Partitions",
+// 		type: "number",
+// 		width: 150,
+// 	},
+// 	{
+// 		field: "partitionRep",
+// 		headerName: "Replicas per partition",
+// 		type: "number",
+// 		width: 150,
+// 	},
+// 	{
+// 		field: "underMinISR",
+// 		headerName: "Under Min ISR",
+// 		type: "number",
+// 		width: 150,
+// 	},
+// 	{ field: "logSize", headerName: "Log Size(GB)", type: "number", width: 120 },
+// 	{
+// 		field: "delete",
+// 		headerName: "",
+// 		filterable: false,
+// 		align: "center",
+// 		width: 180,
+// 		renderCell: (params) => {
+// 			return params.value ? (
+// 				<ConfirmationDialog
+// 					title={`Delete ${params.row.topic}?`}
+// 					content={`Are you sure you want to delete topic: ${params.row.topic}? \n
+//         Enter the topic name below to confirm. This is irreversible!`}
+// 					label='Delete'
+// 					actions={DELETE_TOPIC}
+// 					control={params.row.topic}
+// 					args={{ name: params.row.topic }}
+// 					variant='contained'
+// 					color='error'
+// 					cta='DELETE'
+// 					disabled={!params.value}
+// 					update={params.row.update}
+// 				/>
+// 			) : (
+// 				<ConfirmationDialog
+// 					title={`Delete ${params.row.topic}?`}
+// 					content={`Are you sure you want to delete topic: ${params.row.topic}? \n
+//         Enter the topic name below to confirm. This is irreversible!`}
+// 					label='Delete'
+// 					actions={DELETE_TOPIC}
+// 					control={params.row.topic}
+// 					args={{ name: params.row.topic }}
+// 					variant='contained'
+// 					color='error'
+// 					cta='DELETE'
+// 					disabled={params.value}
+// 					update={params.row.update}
+// 				/>
+// 			);
+// 		},
+// 	},
+// 	{
+// 		field: "reassignPartitions",
+// 		headerName: "",
+// 		filterable: false,
+// 		align: "center",
+// 		width: 180,
+// 	},
+// ];
 
 interface TopicGridProps {
   title?: string;
@@ -79,6 +89,7 @@ interface TopicGridProps {
 export default function TopicGrid({ title, rowCount }: TopicGridProps) {
   const [rowData, setRowData] = useState([]);
   const [pageSize, setPageSize] = useState(rowCount);
+  const navigate = useNavigate();
   const { loading, data, refetch } = useQuery(TOPIC_DATAGRID_QUERY);
 
   React.useEffect(() => {
@@ -100,6 +111,91 @@ export default function TopicGrid({ title, rowCount }: TopicGridProps) {
       setRowData(newRowData);
     }
   }, [data]);
+  const columns: GridColDef[] = [
+    { field: "topic", headerName: "Topic", width: 150 },
+    {
+      field: "partitionNum",
+      headerName: "Number of Partitions",
+      type: "number",
+      width: 150,
+    },
+    {
+      field: "partitionRep",
+      headerName: "Replicas per partition",
+      type: "number",
+      width: 150,
+    },
+    {
+      field: "underMinISR",
+      headerName: "Under Min ISR",
+      type: "number",
+      width: 150,
+    },
+    {
+      field: "logSize",
+      headerName: "Log Size(GB)",
+      type: "number",
+      width: 120,
+    },
+    {
+      field: "delete",
+      headerName: "",
+      filterable: false,
+      align: "center",
+      width: 180,
+      renderCell: (params) => {
+        return params.value ? (
+          <ConfirmationDialog
+            title={`Delete ${params.row.topic}?`}
+            content={`Are you sure you want to delete topic: ${params.row.topic}? \n
+        Enter the topic name below to confirm. This is irreversible!`}
+            label="Delete"
+            actions={DELETE_TOPIC}
+            control={params.row.topic}
+            args={{ name: params.row.topic }}
+            variant="contained"
+            color="error"
+            cta="DELETE"
+            disabled={!params.value}
+            update={params.row.update}
+          />
+        ) : (
+          <ConfirmationDialog
+            title={`Delete ${params.row.topic}?`}
+            content={`Are you sure you want to delete topic: ${params.row.topic}? \n
+        Enter the topic name below to confirm. This is irreversible!`}
+            label="Delete"
+            actions={DELETE_TOPIC}
+            control={params.row.topic}
+            args={{ name: params.row.topic }}
+            variant="contained"
+            color="error"
+            cta="DELETE"
+            disabled={params.value}
+            update={params.row.update}
+          />
+        );
+      },
+    },
+    {
+      field: "reassignPartitions",
+      headerName: "",
+      filterable: false,
+      align: "center",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <Button
+            variant="contained"
+            color="info"
+            onClick={() => navigate(`../reassign/${params.row.topic}`)}
+          >
+            REASSIGN PARTITIONS
+          </Button>
+        );
+      },
+    },
+  ];
 
   return (
     <div style={{ height: "100%" }}>
